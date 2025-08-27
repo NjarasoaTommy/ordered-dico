21514227e74de95b44d65f200aaabda7c169d0f7
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-08-27 20:54:48 +0300

    Create the class for the ordered list and manage the 3 way for initializing the dictionnary

---
+++ dico.py
@@ -0,0 +1,29 @@
+class Dico(dict):
+    
+
+    def __init__(self,*dic, **items):
+        if len(dic) == 0 and len(items) == 0:
+            self.keys_array = []
+            self.values_array = []
+        elif len(dic) == 1 and len(items) == 0:
+            self.keys_array = dic[0].keys()
+            self.values_array = dic[0].values()
+        elif len(dic) == 0 and len(items) > 0:
+            self.keys_array = items.keys()
+            self.values_array = items.values()
+        else:
+            print("Paramètre incorrect")
+            exit(0)
+        print(self.keys_array)
+        print(self.values_array)
+
+
+
+print("=============================================================")
+dic = {"matiere": "anglais", "prof": "Bertin"}
+d = Dico(dic)
+print("===================")
+d2 = Dico(nom="Koto", age=42)
+print("===================")
+d2 = Dico(dic, nom="Koto", age=42)
+print("=============================================================")
