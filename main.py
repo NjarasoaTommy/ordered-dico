6a7612f7bceb3b74ff2326973d49b7d4406b96b0
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-08-27 21:32:44 +0300

    Manage the length of the ordered dictionnary

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from ordered.dictionnary.dico import Dico
 
-print("=============================================================")
 dic = {"matiere": "anglais", "prof": "Bertin"}
 d = Dico(dic)
-print("===================")
-d2 = Dico(nom="Koto", age=42)
-print("===================")
-d2 = Dico(dic, nom="Koto", age=42)
-print("=============================================================")
+
+print(len(d))
--- ordered/dictionnary/dico.py
+++ ordered/dictionnary/dico.py
@@ -5,14 +5,20 @@ class Dico(dict):
         if len(dic) == 0 and len(items) == 0:
             self.keys_array = []
             self.values_array = []
+            self.length = 0
         elif len(dic) == 1 and len(items) == 0:
             self.keys_array = dic[0].keys()
             self.values_array = dic[0].values()
+            self.length = len(dic[0])
         elif len(dic) == 0 and len(items) > 0:
             self.keys_array = items.keys()
             self.values_array = items.values()
+            self.length = len(items)
         else:
             print("Paramètre incorrect")
             exit(0)
         print(self.keys_array)
         print(self.values_array)
+
+    def __len__(self):
+        return self.length
