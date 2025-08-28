379f779cfb1ea61fd183dd9612163c41baf892ac
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-08-28 10:28:42 +0300

    Test the sixth functionnality

--- test.py
+++ test.py
@@ -25,3 +25,12 @@ print("========================================")
 del fruits['haricot']
 print('haricot' in fruits)
 print(legumes['haricot'])
+
+print("========================================")
+for cle in legumes:
+     print(cle)
+
+print(legumes.keys())
+print(legumes.values())
+for nom, qtt in legumes.items():
+    print("{0} ({1})".format(nom, qtt))
