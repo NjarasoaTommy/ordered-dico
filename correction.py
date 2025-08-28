9ff7c61cbe4fe4739b8e22fdbe5c6c37018ae58b
Njarasoa Tommy <njarasoatommy@gmail.com>
2025-08-28 13:34:03 +0300

    Add other proposition to solve the problem

---
+++ correction.py
@@ -0,0 +1,42 @@
+class Dico:
+    def __init__(self):
+        self._cles = []
+        self._valeurs = []
+
+    def __setitem__(self, key, value):
+        if key in self._cles:
+            self._valeurs[self._cles.index(key)] = value
+        else:
+            self._cles.append(key)
+            self._valeurs.append(value)
+
+    def __str__(self):
+        if len(self._cles) == 0:
+            return '{}'
+        s =  '{ "' + str(self._cles[0]) + "\" : "
+        if isinstance(self._valeurs[0], str):
+            s += "\"" + str(self._valeurs[0]) + '"'
+        else:
+            s += str(self._valeurs[0])
+        i = 1
+        while i < len(self._cles):
+            s +=  ", \"" + str(self._cles[i]) + "\" : "
+            if isinstance(self._valeurs[i], str):
+                s += "\"" + str(self._valeurs[i]) + '"'
+            else:
+                s += str(self._valeurs[i])
+            i += 1
+        s += ' }'
+        return s
+    
+    def __repr__(self):
+        return self.__str__(self)
+
+d = Dico()
+d["nom"] = "TOM"
+d["age"] = 12
+d["sexe"] = 'Homme'
+d["nom"] = "Blaise"
+print(d)
+print(d._cles)
+print(d._valeurs)
